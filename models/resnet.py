eac89bb494d732b8c08b5fc55831a2a41eb8e122
Marco <mirco.masera@gmail.com>
2024-02-08 11:00:38 +0100

    Bug fix

--- models/resnet.py
+++ models/resnet.py
@@ -18,6 +18,8 @@ class BaseResNet18(nn.Module):
 def hook_activation_shaping(model: BaseResNet18, alpha=0.5, every_n_convolution=1, skip_first_n_layers=0):
     
     def M_random_generator(shape, alpha):
+        #Generate a random matrix M with values 0 or 1
+        M = torch.ones(shape)
         M = torch.where(torch.rand(shape) <= alpha, M, torch.zeros(shape))
         M = M.to(CONFIG.device, non_blocking=False)
         return M
@@ -26,11 +28,10 @@ def hook_activation_shaping(model: BaseResNet18, alpha=0.5, every_n_convolution=
     def get_activation_shaping_hook(generate_M, alpha):
         def activation_shaping_hook(module, input, output):
             #Apply a transformation to the output
-            #Output shape: [ batch_size, n_filters, n, n ]
             #Binarize output: if < 0 -> 0, else -> 1
             activation = torch.where(output <= 0, torch.zeros_like(output), torch.ones_like(output))
             #Multiply output with random M
-            output = torch.mul(activation, generate_M(activation.shape))
+            output = torch.mul(activation, generate_M(activation.shape, alpha))
             #Return output
             return output
         return activation_shaping_hook
@@ -38,7 +39,6 @@ def hook_activation_shaping(model: BaseResNet18, alpha=0.5, every_n_convolution=
     #Make a list of all network convolutional layers to easily iterate over them
     all_layers = []
 
-    #all_layers.append(model.resnet.conv1)
     for layer_group in [model.resnet.layer1, model.resnet.layer2, model.resnet.layer3, model.resnet.layer4]:
         for layer in layer_group:
             all_layers.append(layer.conv1)
@@ -47,7 +47,6 @@ def hook_activation_shaping(model: BaseResNet18, alpha=0.5, every_n_convolution=
     n_applied = 0
     for i, layer in enumerate(all_layers):
         if (i >= skip_first_n_layers) and (every_n_convolution==0 or ((i-skip_first_n_layers) % every_n_convolution == 0)):
-            #M = generate_M([layer.out_channels, output_size,output_size])
             layer.register_forward_hook(get_activation_shaping_hook(M_random_generator, alpha))
             n_applied += 1
     print(f"Applied to {n_applied} layers")
